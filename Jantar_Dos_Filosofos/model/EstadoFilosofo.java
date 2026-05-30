2928421ad9d9061481d49c61fd3a878e471ccb2c
Maxsuel Santos <maxsuel.ap.l.santos@gmail.com>
2026-05-29 21:35:44 -0300

    Enum EstadoFilosofo

--- Jantar_Dos_Filosofos/model/EstadoFilosofo.java
+++ Jantar_Dos_Filosofos/model/EstadoFilosofo.java
@@ -1,5 +1,29 @@
+/* *********************************************************************
+* Autor............: Maxsuel Aparecido Lima Santos
+* Matricula........: 202511587
+* Inicio...........: 29/05/2026
+* Ultima alteracao.: 29/05/2026
+* Nome.............: EstadoFilosofo.java
+* Funcao...........: Enum que representa os 3 estados possiveis de
+*                    cada filosofo conforme o pseudocodigo do livro
+*                    de Tanenbaum:
+*                      #define THINKING 0
+*                      #define HUNGRY   1
+*                      #define EATING   2
+************************************************************************ */
 package Jantar_Dos_Filosofos.model;
 
+/* ***************************************************************
+* Enum: EstadoFilosofo
+* Funcao: Mapeia os tres estados do pseudocodigo C para Java.
+*         PENSANDO = THINKING = 0
+*         FAMINTO  = HUNGRY   = 1
+*         COMENDO  = EATING   = 2
+*************************************************************** */
 public enum EstadoFilosofo {
-    
-}
+
+  PENSANDO,
+  FAMINTO,
+  COMENDO
+  
+} // Fim do enum EstadoFilosofo
