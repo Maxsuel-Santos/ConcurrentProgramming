6c443a15ebed420449fb3f7dfee14af70de962cd
Maxsuel Santos <maxsuel.ap.l.santos@gmail.com>
2026-06-08 19:47:10 -0300

    Enum das threads

--- Leitores_Escritores/model/EstadoThread.java
+++ Leitores_Escritores/model/EstadoThread.java
@@ -1,5 +1,23 @@
+/* *********************************************************************
+* Autor............: Maxsuel Aparecido Lima Santos
+* Matricula........: 202511587
+* Inicio...........: 08/06/2026
+* Ultima alteracao.: 08/06/2026
+* Nome.............: EstadoThread.java
+* Funcao...........: Enum que representa os 4 estados possiveis 
+*                    de uma Thread/Processo
+************************************************************************ */
 package model;
 
+/* ***************************************************************
+* Enum: EstadoThread
+* Funcao: Mapeia os 4 estados possíveis de uma thread/processo
+*************************************************************** */
 public enum EstadoThread {
-    
-}
+
+    OCIOSO,
+    AGUARDANDO,
+    ATIVO,
+    PAUSADO
+
+} // Fim do enum EstadoThread
