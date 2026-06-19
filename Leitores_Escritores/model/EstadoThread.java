2eb000d30c1fb5fb8f3d8150b90e34a995b0b633
Maxsuel Santos <maxsuel.ap.l.santos@gmail.com>
2026-06-18 23:51:55 -0300

    Removendo acentuação

--- Leitores_Escritores/model/EstadoThread.java
+++ Leitores_Escritores/model/EstadoThread.java
@@ -11,7 +11,7 @@ package model;
 
 /* ***************************************************************
 * Enum: EstadoThread
-* Funcao: Mapeia os 4 estados possíveis de uma thread/processo
+* Funcao: Mapeia os 4 estados possiveis de uma thread/processo
 *************************************************************** */
 public enum EstadoThread {
 
