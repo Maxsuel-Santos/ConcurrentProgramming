efced9d47b3fd89f833ef9b83d4a8b8aa994cda0
Maxsuel Santos <maxsuel.ap.l.santos@gmail.com>
2026-06-22 14:06:07 -0300

    Implementação da TelaInicial do projeto

--- Transito_Automato/Principal.java
+++ Transito_Automato/Principal.java
@@ -1,3 +1,63 @@
-public class Principal {
+/* *********************************************************************
+* Autor............: Maxsuel Aparecido Lima Santos
+* Matricula........: 202511587
+* Inicio...........: 22/06/2026
+* Ultima alteracao.: 22/06/2026
+* Nome.............: Principal.java
+* Funcao...........: Ponto de entrada da aplicacao JavaFX.
+*                    Carrega a tela inicial (TelaInicial.fxml) e
+*                    configura o fechamento correto da aplicacao.
+************************************************************************ */
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import controller.TelaInicialController;
+import controller.SimulacaoController;
+
+@SuppressWarnings("unused")
+public class Principal extends Application {
     
+  /* ***************************************************************
+  * Metodo: start
+  * Funcao: Metodo de entrada do JavaFX. Carrega TelaInicial.fxml
+  *         e exibe a janela principal.
+  * Parametros: @param stage janela principal fornecida pelo JavaFX
+  * Retorno: void
+  *************************************************************** */
+  @Override
+  public void start(Stage stage) throws Exception {
+
+    Parent root = FXMLLoader.load(
+        getClass().getResource("/view/TelaInicial.fxml")
+    );
+
+    stage.setTitle("Grande Transito Automato");
+    stage.setScene(new Scene(root));
+    stage.setResizable(false);
+
+    // Encerra JVM e threads daemon ao fechar a janela
+    stage.setOnCloseRequest(e -> {
+      Platform.exit();
+      System.exit(0);
+    });
+
+    stage.show();
+
+  } // Fim do metodo start
+
+  /* ***************************************************************
+  * Metodo: main
+  * Funcao: Ponto de entrada da aplicacao.
+  * Parametros: @param args argumentos da linha de comando
+  * Retorno: void
+  *************************************************************** */
+  public static void main(String[] args) {
+    launch(args);
+  } // Fim do metodo main
+
 }
--- Transito_Automato/controller/TelaInicialController.java
+++ Transito_Automato/controller/TelaInicialController.java
@@ -1,5 +1,49 @@
+/* *********************************************************************
+* Autor............: Maxsuel Aparecido Lima Santos
+* Matricula........: 202511587
+* Inicio...........: 22/06/2026
+* Ultima alteracao.: 22/06/2026
+* Nome.............: TelaInicialController.java
+* Funcao...........: Controller da tela (fxml) telaInicial que coordena 
+*                    os eventos.
+************************************************************************ */
+
 package controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class TelaInicialController {
     
+    /* ***************************************************************
+    * Metodo: iniciarJogo
+    * Funcao: Responsavel por redirecionar o usuario a tela de simulacao
+    *         do Transito Automato.
+    * Parametros: @param event evento de clique
+    * Retorno: sem retorno
+    *************************************************************** */
+    @FXML
+    private void iniciarTransito(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(
+            getClass().getResource("/view/Simulacao.fxml")
+        );
+
+        Stage stage = (Stage) ((Node) event.getSource())
+                .getScene()
+                .getWindow();
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+
+        stage.show();
+    } // Fim do metodo iniciarTransito    
+
 }
